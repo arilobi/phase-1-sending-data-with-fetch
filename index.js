26ea40b116f1c7e0893967a57eecc3937c79f3c4
Marion Nabulobi <arinabulobi@gmail.com>
2024-10-23 00:35:54 +0300

    add sending data

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Add your code here
+function submitData(name, email) {
+    
+    return fetch("http://localhost:3000/users", {
+        method: "POST", 
+        headers: {
+            "Content-Type": "application/json", 
+            "Accept": "application/json" 
+        },
+        body: JSON.stringify({
+            name: name, 
+            email: email 
+        })
+    })
+    .then(response => {
+        return response.json(); 
+    })
+    .then(data => {
+    
+        const body = document.querySelector('body');
+        const newId = document.createElement('p');
+        newId.textContent = `New ID: ${data.id}`;
+        body.appendChild(newId);
+    })
+    .catch(error => {
+
+        // Handling errors 
+        const body = document.querySelector('body');
+        const errorMessage = document.createElement('p');
+        errorMessage.textContent = `Error: ${error.message}`;
+        body.appendChild(errorMessage);
+    });
+}
+
